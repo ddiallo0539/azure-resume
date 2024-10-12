608e2ae02e50a5b79fa34025fb6f091c4a388663
root <root@localhost.localdomain>
2024-10-12 12:55:17 -0400

    premier commit

---
+++ frontend/main.js
@@ -0,0 +1,17 @@
+windows.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+    }).catch(function(error){
+        console.log(error)
+    });
+    return count;
+}
